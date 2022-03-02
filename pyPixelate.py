464420f7749bc985c5134d0c38450a2fcafd1f2d
MCTVR <69135058+MCTVR@users.noreply.github.com>
2022-03-02 23:12:04 +0800

    Create pyPixelate.py

---
+++ pyPixelate.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+from multiprocessing import Pool
+from sys import argv
+
+def makePixelate(img, output_img, imheight, imwidth, width, height, block_size, fileName):
+    for i in range(0, imheight // block_size):
+        for j in range(0, imwidth // block_size):
+
+            x, x2 = j * width, (j + 1) * width
+            y, y2 = i * height, (i + 1) * height
+
+            a = img[y:y2, x:x2]
+            a = np.array(a)
+
+            mean_color = np.mean(a, axis=(0, 1))
+            mean_color = tuple([int(mean_color[0]), int(mean_color[1]), int(mean_color[2])])
+
+            a = np.ones((height-2, width-2, 3), np.uint8)*mean_color
+            a = cv2.copyMakeBorder(a, 1, 1, 1, 1, borderType=cv2.BORDER_CONSTANT,value=0)
+
+            output_img[y:y2, x:x2] = a
+
+    cv2.imwrite(fileName, output_img)
+
+def pixelate(img, block_size, fileName):
+    img = cv2.imread(img)
+    img = cv2.resize(img, (2000,2000), interpolation = cv2.INTER_LINEAR)
+    imwidth, imheight = img.shape[:2]
+
+    height = imheight // (imheight // block_size)
+    width = imwidth // (imwidth // block_size)
+    
+    output_img = np.ones((2000,2000,3), np.uint8)*255
+
+    pool = Pool(16)
+    pool.starmap(makePixelate, [(img, output_img, imheight, imwidth, width, height, block_size, fileName)])
+    pool.close()
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: python3 pyPixelate.py <image> <block size (can be divided with 2000)> <output image (e.g. 1.jpg)>")
+        exit(1)
+    pixelate(argv[1], int(argv[2]), argv[3])
